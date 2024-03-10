9688fdd0401dc190415fbaeb23c05bf48d0eaca7
Johnes Sombra <jhonesk5@gmail.com>
2024-03-09 21:23:59 -0500

    Biblioteca Designer

---
+++ WebApp/biblioteca/js/biblioteca.js
@@ -0,0 +1,5 @@
+function mostrarApostilas(categoria) {
+    // Aqui você pode implementar a lógica para carregar os arquivos HTML da categoria selecionada
+    // Por enquanto, vamos apenas mostrar uma mensagem de exemplo
+    document.getElementById('apostilas').innerHTML = `<p>Mostrando apostilas da categoria: ${categoria}</p>`;
+}
